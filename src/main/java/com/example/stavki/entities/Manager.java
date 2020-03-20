7b836fb38cbc8de935bf1a73314ac719cce921c7
MaxXxXxim123 <56824195+MaxXxXxim123@users.noreply.github.com>
2020-03-20 20:44:33 +0300

    Create Manager.java

---
+++ src/main/java/com/example/stavki/entities/Manager.java
@@ -0,0 +1,27 @@
+package com.example.stavki.entities;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Manager extends User{
+    private @Id @GeneratedValue Long id;
+
+    Manager(){}
+
+    Manager(String name){
+        super(name);
+    }
+
+    public void pay(){
+        //логика
+    }
+
+    public void addStake(Stake stake){
+        //логика
+    }
+}
