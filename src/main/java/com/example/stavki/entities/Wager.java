900c72bbeaac6c97a927350357dbd2c1e0fed79c
MaxXxXxim123 <56824195+MaxXxXxim123@users.noreply.github.com>
2020-03-20 22:12:48 +0300

    Update Wager.java

--- src/main/java/com/example/stavki/entities/Wager.java
+++ src/main/java/com/example/stavki/entities/Wager.java
@@ -6,12 +6,22 @@ import javax.persistence.Entity;
 import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.math.BigDecimal;
 
 @Data
 @Entity
 public class Wager {
-    @ForeignKey
-    private Stake stakeFK;
+    private @Id Long gameFK;
+    private @Id Long clientFK;
+    private BigDecimal money;
+    private boolean isFirstTeamWinning;
 
+    Wager(){}
 
+    public Wager(Client client, Game game, BigDecimal money, boolean isFirstTeamWinning){
+        this.gameFK = game.getId();
+        this.clientFK = client.getId();
+        this.money = money;
+        this.isFirstTeamWinning = isFirstTeamWinning;
+    }
 }
