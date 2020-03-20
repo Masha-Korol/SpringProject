4ee6df6fa25d76c7481e00ec25d84cc4b5f769f9
MaxXxXxim123 <56824195+MaxXxXxim123@users.noreply.github.com>
2020-03-20 20:44:20 +0300

    Create StavkiApplication.java

---
+++ src/main/java/com/example/stavki/StavkiApplication.java
@@ -0,0 +1,13 @@
+package com.example.stavki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StavkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StavkiApplication.class, args);
+	}
+
+}
