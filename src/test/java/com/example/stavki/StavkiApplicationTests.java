8b20ba6209e30433b42f8e16f012fd8654861f02
MaxXxXxim123 <56824195+MaxXxXxim123@users.noreply.github.com>
2020-03-20 20:44:17 +0300

    Create StavkiApplicationTests.java

---
+++ src/test/java/com/example/stavki/StavkiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stavki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StavkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
