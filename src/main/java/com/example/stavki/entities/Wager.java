41c0541c0d058cd060e6e60349a21242842814a9
MaxXxXxim123 <56824195+MaxXxXxim123@users.noreply.github.com>
2020-03-21 18:17:13 +0300

    change

--- src/main/java/com/example/stavki/entities/Wager.java
+++ src/main/java/com/example/stavki/entities/Wager.java
@@ -11,6 +11,7 @@ import java.math.BigDecimal;
 @Data
 @Entity
 public class Wager {
+    private @Id @GeneratedValue Long id;
     private @Id Long gameFK;
     private @Id Long clientFK;
     private BigDecimal money;
