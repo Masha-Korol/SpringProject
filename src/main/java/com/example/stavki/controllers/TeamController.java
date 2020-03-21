cf8b82a9235f5e4912017077a4d5cdf00133a0bb
MaxXxXxim123 <56824195+MaxXxXxim123@users.noreply.github.com>
2020-03-21 19:32:15 +0300

    Create TeamController.java

---
+++ src/main/java/com/example/stavki/controllers/TeamController.java
@@ -0,0 +1,86 @@
+package com.example.stavki.controllers;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+// tag::hateoas-imports[]
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import com.example.stavki.entities.Team;
+import com.example.stavki.repos.TeamRepository;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+// end::hateoas-imports[]
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TeamController {
+
+    private final TeamRepository repository;
+
+    TeamController(TeamRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/teams")
+    CollectionModel<EntityModel<Team>> all() {
+
+        List<EntityModel<Team>> teams = repository.findAll().stream()
+                .map(team -> new EntityModel<>(team,
+                        linkTo(methodOn(TeamController.class).one(team.getName())).withSelfRel(),
+                        linkTo(methodOn(TeamController.class).all()).withRel("teams")))
+                .collect(Collectors.toList());
+
+        return new CollectionModel<>(teams,
+                linkTo(methodOn(TeamController.class).all()).withSelfRel());
+    }
+
+
+    @PostMapping("/teams")
+    Team newTeam(@RequestBody Team newTeam) {
+        return repository.save(newTeam);
+    }
+
+
+    @GetMapping("/teams/{id}")
+    EntityModel<Team> one(@PathVariable String id) {
+
+        Team team = repository.findById(id)
+                .orElseThrow(() -> new TeamNotFoundException(id));
+
+        return new EntityModel<>(team,
+                linkTo(methodOn(TeamController.class).one(id)).withSelfRel(),
+                linkTo(methodOn(TeamController.class).all()).withRel("teams"));
+    }
+
+
+    @PutMapping("/teams/{id}")
+    Team replaceTeam(@RequestBody Team newTeam, @PathVariable String id) {
+
+        return repository.findById(id)
+                .map(team -> {
+                    team.setHistoryOfVictories(newTeam.getHistoryOfVictories());
+                    team.setWinningRate(newTeam.getWinningRate());
+                    team.setName(newTeam.getName());
+
+
+                    return repository.save(team);
+                })
+                .orElseGet(() -> {
+                    newTeam.setName(id);
+                    return repository.save(newTeam);
+                });
+    }
+
+    @DeleteMapping("/teams/{id}")
+    void deleteTeam(@PathVariable String id) {
+        repository.deleteById(id);
+    }
+}
